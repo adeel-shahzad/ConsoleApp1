df6f4b851125b7b39bf8c60840e2c3598670aaa3
Adeel Shahzad <adeel-shahzad@users.noreply.github.com>
2023-09-24 22:04:18 +0500

    added prototype dp

---
+++ DesignPattern/Prototype/House.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Creational.Prototype
+{
+    internal class House : IPrototypeHouse
+    {
+        public string WindowsType { get; set; }
+
+        public IPrototypeHouse GetClone()
+        {
+            var house = new House();
+            house.WindowsType = "wooden";
+            return house;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using DesignPattern.Creational.FactoryMethod.Client;
 using DesignPattern.Creational.AbstractFactory.Client;
 using DesignPattern.Creational.Builder.Client;
+using DesignPattern.Creational.Prototype;
 
 new FactoryMethodClient();
 new AbstractFactoryClient();
 new BuilderClient();
+
+new PrototypeClient();
